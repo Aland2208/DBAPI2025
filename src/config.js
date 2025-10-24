da94347ba94ab18d7e23e9a7cdf30a9251c41219
Aland2208 <alandloor22@gmail.com>
2025-10-23 22:34:25 -0500

    actualizado6

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 import {config} from 'dotenv'
 config()
 
-export const BD_HOST=process.env.BD_HOST || 'localhost'
-export const BD_DATABASE=process.env.BD_DATABASE || 'baseapp20252'
-export const BD_USER=process.env.BD_USER ||'root'
-export const BD_PASSWORD=process.env.BD_PASSWORD || ''
+export const BD_HOST=process.env.BD_HOST || 'bu4jzgpw41nahtefkqe6-mysql.services.clever-cloud.com'
+export const BD_DATABASE=process.env.BD_DATABASE || 'bu4jzgpw41nahtefkqe6'
+export const BD_USER=process.env.BD_USER ||'uj3iqphebuhng4wm'
+export const BD_PASSWORD=process.env.BD_PASSWORD || 'kIfwbeCuTqCSifyVST5o'
 export const BD_PORT=process.env.BD_PORT || 3306
 export const PORT=process.env.PORT || 3000
 
