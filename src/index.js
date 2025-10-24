6631b351584250341d244f7ee30e7f8e42cdc048
Aland2208 <alandloor22@gmail.com>
2025-10-23 22:24:07 -0500

    actualizado4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import app from './app.js'
 import{PORT} from './config.js'
 
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
-});
+app.listen(PORT);//3000
+console.log('El servidor esta escuchando por el puesto:',PORT)
